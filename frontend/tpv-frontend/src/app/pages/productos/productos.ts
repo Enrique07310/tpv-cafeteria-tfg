8607cb5f0b968652a58dc751cc9797055d4d9bf6
enrique <ecerqueira1609@gmail.com>
2026-06-02 10:12:55 +0200

    Mover boton

--- frontend/tpv-frontend/src/app/pages/productos/productos.ts
+++ frontend/tpv-frontend/src/app/pages/productos/productos.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 import { ProductosService } from '../../services/productos';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-productos',
@@ -14,20 +16,68 @@ export class Productos implements OnInit {
 
   productos: any[] = [];
 
-  constructor(private productosService: ProductosService) {}
+  private apiUrl = `${environment.apiUrl}/productos`;
+
+  constructor(
+    private productosService: ProductosService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.cargarProductos();
   }
 
   cargarProductos() {
+
     this.productosService.obtenerProductos().subscribe({
+
       next: (data) => {
         this.productos = data;
       },
+
       error: (error) => {
         console.error('Error cargando productos', error);
       }
+
     });
+
   }
+
+  //  REPONER STOCK
+  reponerStock(producto: any) {
+
+    const cantidad =
+      prompt('¿Cuánto stock quieres añadir?');
+
+    if (!cantidad) return;
+
+    const nuevoStock =
+      producto.stock + Number(cantidad);
+
+    this.http.put(
+      `${this.apiUrl}/${producto.id}/stock`,
+      {
+        stock: nuevoStock
+      }
+    ).subscribe({
+
+      next: () => {
+
+        producto.stock = nuevoStock;
+
+      },
+
+      error: (error) => {
+
+        console.error(
+          'ERROR REPONIENDO STOCK',
+          error
+        );
+
+      }
+
+    });
+
+  }
+
 }
